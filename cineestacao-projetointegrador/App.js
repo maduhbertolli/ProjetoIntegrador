f5516600fd119bec60a3ad7aa7952ce1b5a82477
maduhbertolli <maria.e.silva797@aluno.senai.br>
2026-09-09 14:19:12 -0300

    atualização do app.js com navegação

--- cineestacao-projetointegrador/App.js
+++ cineestacao-projetointegrador/App.js
@@ -1,7 +1,67 @@
-import ListaFilmesScreen from './src/screens/ListarFilmesSecreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import AdicionarFilmesScreen from './src/screens/AdicionarFilmesScreen';
+import ListarFilmesScreen from './src/screens/ListarFilmesScreen';
+import DetalhesFilmesScreen from './src/screens/DetalhesFilmesScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ListaFilmesScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ListaFilmes">
+        <Stack.Screen
+          name="ListaFilmes"
+          component={ListarFilmesScreen}
+          options={{ 
+            title: 'Meus Filmes',
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#f5f5f5'
+            },
+            headerTintColor: '#171717',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20
+            }
+          }}
+        />
+
+        <Stack.Screen
+          name="AdicionarFilme"
+          component={AdicionarFilmesScreen}
+          options={{ 
+            title: 'Avaliar Filme',
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#f5f5f5'
+            },
+            headerTintColor: '#171717',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20
+            }
+          }}
+        />
+
+        <Stack.Screen
+          name="Detalhes"
+          component={DetalhesFilmesScreen}
+          options={{ 
+            title: 'Detalhes do Filme',
+            headerTitleAlign: 'center',
+            headerStyle: {
+              backgroundColor: '#f5f5f5'
+            },
+            headerTintColor: '#171717',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
